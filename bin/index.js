633ccf3f05e9d3566e55cb39346dc25c7b5110be
Link <chavarro@google.com>
2019-02-01 23:52:24 -0500

    add new-branch script

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,47 @@
-console.log('lazy commit init')
+require('colors');
+const simpleGit = require('simple-git')();
+
+const script = process.argv[2];
+
+const globalConfig = require('./config.json');
+
+const scripts = {
+  'new-branch': createBranch,
+}
+
+const {
+  errorHandler,
+  generateQuestion,
+  parseOutput,
+} = require('./utils');
+
+/**
+ * Create branch with the name based on the ticket id and title slugified
+ */
+function createBranch() {
+  let abort = false;
+  const config = globalConfig['new-branch'];
+  const {
+    questions,
+    output,
+  } = config;
+  const variables = {};
+
+  questions.map((question) => {
+    variables[question.id] = generateQuestion(question);
+
+    if (variables[question.id] === null) {
+      abort = true;
+    }
+  });
+
+  if (abort) {
+    return;
+  }
+
+  const branchName = parseOutput(output, variables);
+
+  simpleGit.checkoutLocalBranch(branchName);
+}
+
+scripts[script]()

5ef17af8bd462db5d70e9f53a25067dc6d3d6c2a
Link <chavarro@google.com>
2019-02-08 22:01:17 -0500

    add commit task

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const script = process.argv[2];
 const globalConfig = require('./config.json');
 
 const scripts = {
+  'commit': require('./lib/tasks/commit'),
   'new-branch': require('./lib/tasks/new-branch'),
-}
+};
 
 scripts[script](globalConfig);

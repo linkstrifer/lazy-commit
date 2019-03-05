625719e86fdf11a5909deb44b5ceaaa57b4946b5
Link <chavarro@google.com>
2019-03-04 22:08:06 -0500

    add project config require

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
 require('colors');
 
 const script = process.argv[2];
+const loadConf = require('app-root-path').require;
+let globalConfig;
 
-const globalConfig = require('./config.json');
+try {
+  // globalConfig = loadConf('/lazy-commit.config.json');
+} catch (error) {
+  try {
+    // globalConfig = require('./config.json');
+  }
+  catch (error) {
+    console.log('No config file'.red);
+  }
+}
 
 const scripts = {
   'commit': require('./lib/tasks/commit'),
   'new-branch': require('./lib/tasks/new-branch'),
 };
 
-scripts[script](globalConfig);
+if (globalConfig) {
+  scripts[script](globalConfig);
+}

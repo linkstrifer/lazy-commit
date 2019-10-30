f7d7ca3a27c6e07c926ae81d13f9e49145a658fe
Link Strifer <chavarro@google.com>
2019-10-29 19:58:22 -0500

    feature}
    Add multiple option support, refactor options to support javascript expressions, remove filters, fix tests

--- test/shared/utils/utils.parseTemplate.test.js
+++ test/shared/utils/utils.parseTemplate.test.js
@@ -1,57 +1,38 @@
 const assert = require('assert')
 
-const template = '${a}/${b|slug}'
+const template = '${a}/${b}'
 const variables = {
   a: 'variableA',
   b: 'variable-B',
 }
 
-const nullParsedTemplate = '/'
-
-const {
-  parseTemplate,
-} = require('../../../lib/shared/utils')
+const { parseTemplate } = require('../../../lib/shared/utils')
 
 describe('ParseTemplate', function() {
   it('Should parse a template', function() {
     assert.equal(
-      parseTemplate(template,variables),
+      parseTemplate(template, variables),
       `${variables.a}/${variables.b}`
     )
   })
 
   it('Empty template, should return empty string', function() {
-    assert.equal(
-      parseTemplate('', variables),
-      ''
-    )
+    assert.equal(parseTemplate('', variables), '')
   })
 
   it('Null template, should return empty string', function() {
-    assert.equal(
-      parseTemplate(null, variables),
-      ''
-    )
+    assert.equal(parseTemplate(null, variables), '')
   })
 
   it('Load a string instead of a variables object, should return parsed template with empty string instead of undefined variables', function() {
-    assert.equal(
-      parseTemplate(template, ''),
-      nullParsedTemplate
-    )
+    assert.equal(parseTemplate(template, ''), template)
   })
 
   it('Load a number instead of a variables object, should return parsed template with empty string instead of undefined variables', function() {
-    assert.equal(
-      parseTemplate(template, 1234),
-      nullParsedTemplate
-    )
+    assert.equal(parseTemplate(template, 1234), template)
   })
 
   it('Load null instead of a variables object, should return parsed template with empty string instead of undefined variables', function() {
-    assert.equal(
-      parseTemplate(template, null),
-      nullParsedTemplate
-    )
+    assert.equal(parseTemplate(template, null), template)
   })
 })
